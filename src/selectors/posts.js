cd50daf610845d1ebb16c63c9ce3aa578ebec31c
julam <jlam499@gmail.com>
2018-03-15 16:57:10 -0700

    fixed removal of post from user path in firebase

--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -3,16 +3,20 @@ import moment from 'moment';
 // Get visible posts
 
 export default (posts, { text, sortBy }) => {
-  return posts.filter((post) => {
-    const createdAtMoment = moment(post.createdAt);
-    const textMatch = post.title.toLowerCase().includes(text.toLowerCase());
+  if(posts.length > 0) {
+    return posts.filter((post) => {
+      const createdAtMoment = moment(post.createdAt);
+      const textMatch = post.title.toLowerCase().includes(text.toLowerCase());
 
-    return textMatch;
-  }).sort((a, b) => {
-    if (sortBy === 'date') {
-      return a.createdAt < b.createdAt ? 1 : -1;
-    } else if (sortBy === 'title') {
-      return a.title.localeCompare(b.title);
-    }
-  });
+      return textMatch;
+    }).sort((a, b) => {
+      if (sortBy === 'date') {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      } else if (sortBy === 'title') {
+        return a.title.localeCompare(b.title);
+      }
+    });
+  } else {
+    return posts;
+  }
 };
